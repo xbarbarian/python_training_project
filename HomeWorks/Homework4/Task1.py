468da0b071872f2ed8b41b838adfeb682891df2f
xbarbarian <alexandr@zhukov.pro>
2020-06-25 01:51:18 +0400

    Task 1 Done

---
+++ HomeWorks/Homework4/Task1.py
@@ -0,0 +1,8 @@
+# Home work Lesson 4 Task 1
+
+
+def salary(hours, rate_per_hour, premium):
+    return (hours * rate_per_hour) + premium
+
+
+print('Ваш доход с учетом премии:', salary(8*5*4, 500, 5000), 'р.', sep='')
