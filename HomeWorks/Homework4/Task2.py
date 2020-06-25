7ec99003322fec244327c7c4b92e8808d77c3a7a
xbarbarian <alexandr@zhukov.pro>
2020-06-25 18:48:32 +0400

    Task 2 Done

---
+++ HomeWorks/Homework4/Task2.py
@@ -0,0 +1,17 @@
+
+import random
+
+my_randoms = [random.randrange(0, 100, 10) for _ in range(10)]
+
+print(my_randoms)
+i = 0
+new_list = []
+while i < len(my_randoms) - 1:
+    if my_randoms[i] < my_randoms[i+1]:
+        print(my_randoms[i])
+        new_list.append(my_randoms[i+1])
+        i += 1
+
+    else:
+        i += 1
+print(new_list)
