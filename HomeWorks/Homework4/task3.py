2112020c2332e7908c8d7aa53830d3ca7c02b975
xbarbarian <alexandr@zhukov.pro>
2020-06-25 18:56:45 +0400

    Task 3 Done

---
+++ HomeWorks/Homework4/task3.py
@@ -0,0 +1,3 @@
+# Home Work Lesson 4  Task 3
+# print(f'Числа от 20 до 240 - {[el for el in range(20, 241)]}')
+print(f'Выводим числа от 20 до 240 кратные 20 или 21: {[el for el in range(20, 241) if el % 20 == 0 or el % 21 == 0]}')
