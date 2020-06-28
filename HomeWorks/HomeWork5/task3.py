ae61b26662beb36684ea055bfd2451c8f8f916e5
xbarbarian <alexandr@zhukov.pro>
2020-06-28 21:56:11 +0400

    Task 3 Done

---
+++ HomeWorks/HomeWork5/task3.py
@@ -0,0 +1,18 @@
+# Home Work lesson 5 task 3
+# 3. Создать текстовый файл (не программно),
+# построчно записать фамилии сотрудников и величину их окладов.
+# Определить, кто из сотрудников имеет оклад менее 20 тыс.,
+# вывести фамилии этих сотрудников. Выполнить подсчет средней величины дохода сотрудников.
+
+with open('List_workers.txt') as l_workers:
+    min_salary = []
+    average_salary = []
+    my_list = l_workers.read().split('\n')
+    print(my_list)
+    for i in my_list:
+        i = i.split()
+        if int(i[1]) < 20000:
+            min_salary.append(i[1])
+        average_salary.append(i[1])
+print(f'{min_salary} - {sum(map(int,average_salary))/len(average_salary)}')
+
